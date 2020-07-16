1fabfbf03cfae872de1c37ee66031141fbca9d42
João Miguel <joaomiguel.farias@gmail.com>
2020-07-16 17:28:06 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          João Miguel
         </p>
         <a
           className="App-link"
