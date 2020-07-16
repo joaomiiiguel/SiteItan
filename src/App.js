1333db5c803d79fbb7208cc2cbe22d76e800d95b
João Miguel <joaomiguel.farias@gmail.com>
2020-07-16 17:29:03 -0300

    Novo Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          João Miguel
+          João Miguell
         </p>
         <a
           className="App-link"
